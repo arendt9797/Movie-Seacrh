f0d9592f87ffaeadcc8be0f0f1e593570e955900
hoki9797 <hoki9797@gmail.com>
2025-01-17 11:34:42 +0900

    chore: 배포를 위한 수정

--- js/api/movieData.js
+++ js/api/movieData.js
@@ -1,8 +1,9 @@
-import config from "../../config/apiKey.js"
+// .gitingore를 통해 API key를 숨기고 싶을 경우. 배포를 위해 비활성화
+// import config from "../../config/apiKey.js"
 
 const getMovieData = async (url) => {
     try {
-        const APIKEY = config.apiKey
+        const APIKEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGZiMzRlYzIxMDIwYzEzZDQ3M2Q5MDRiMjAxYTc1MyIsIm5iZiI6MTczNjI5NjkyMy4zMjUsInN1YiI6IjY3N2RjOWRiMTI2Njc5Njg4NTRlNTA1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a5bQzmbRDYQUi4oZnHJuUMnSD6HXe3zE2k3C5etrbLk"
         const options = {
             method: 'GET',
             headers: {
